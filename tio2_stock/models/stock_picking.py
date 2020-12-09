cca71a71031435af8e19ea2e59991ee9c1405969
Yisel García <info.tiodoo@gmail.com>
2020-12-09 17:47:56 +0100

    [UPD] Corrigiendo errores detectados en el servidor de pruebas.

--- tio2_stock/models/stock_picking.py
+++ tio2_stock/models/stock_picking.py
@@ -6,5 +6,5 @@ from odoo import api, fields, models, _
 class StockPickingTO2(models.Model):
     _inherit = "stock.picking"
 
-    code_cliente = fields.Char(related='pack_operation_code_ids.code_cliente')
-    desc_cliente = fields.Char(related='pack_operation_code_ids.desc_cliente')
+    code_cliente = fields.Char(related='pack_operation_product_ids.code_cliente')
+    desc_cliente = fields.Char(related='pack_operation_product_ids.desc_cliente')
