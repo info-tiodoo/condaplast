7d6f9e383a11917077d18906d046ef993cfb4d86
Yisel García <info.tiodoo@gmail.com>
2020-12-09 17:59:18 +0100

    [UPD] Actualizando versiones.

--- tio2_product/__manifest__.py
+++ tio2_product/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Extend Product - Condaplast",
-    'version' : '10.0.1.1.1',
+    'version' : '10.0.1.1.2',
     'description': """
         - Add 'Marca' field to Product variant search views
     """,
